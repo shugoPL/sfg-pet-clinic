5ea9fb929aad5ca446827b93162f89d83862a857
shugoPL <shugoPL@users.noreply.github.com>
2022-11-16 09:45:16 +0100

    Initial project add.

---
+++ src/main/java/pl/com/shugo/ownpetclinic/OwnPetClinicApplication.java
@@ -0,0 +1,13 @@
+package pl.com.shugo.ownpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/com/shugo/ownpetclinic/OwnPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.com.shugo.ownpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
