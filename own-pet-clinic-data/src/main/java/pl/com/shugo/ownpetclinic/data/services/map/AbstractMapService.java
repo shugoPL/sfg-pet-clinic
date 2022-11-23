907ec5295bfe7e0d86c1328a080a5dde6b1b02f0
shugoPL <shugoPL@users.noreply.github.com>
2022-11-23 10:01:57 +0100

    Refactored Map Impl to abstract ID generation. #23

--- own-pet-clinic-data/src/main/java/pl/com/shugo/ownpetclinic/data/services/map/AbstractMapService.java
+++ own-pet-clinic-data/src/main/java/pl/com/shugo/ownpetclinic/data/services/map/AbstractMapService.java
@@ -1,23 +1,30 @@
 package pl.com.shugo.ownpetclinic.data.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import pl.com.shugo.ownpetclinic.data.model.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll() {
-      return new HashSet<>(map.values());
+        return new HashSet<>(map.values());
     }
 
     T findById(ID id) {
         return map.get(id);
     }
 
-    T save(ID id, T obj) {
-        map.put(id, obj);
+    T save(T obj) {
+
+        if (obj != null) {
+            if (obj.getId() == null) {
+                obj.setId(getNextId());
+            }
+            map.put(obj.getId(), obj);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return obj;
     }
 
@@ -27,5 +34,16 @@ public abstract class AbstractMapService<T, ID> {
 
     void delete(T obj) {
         map.entrySet().removeIf(entry -> entry.getValue().equals(obj));
+    }
+
+    private Long getNextId() {
+        Long nextId = null;
+        try {
+            nextId = Collections.max(map.keySet()) + 1;
+        } catch (NoSuchElementException e) {
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
--- own-pet-clinic-web/src/main/java/pl/com/shugo/ownpetclinic/bootstrap/DataLoader.java
+++ own-pet-clinic-web/src/main/java/pl/com/shugo/ownpetclinic/bootstrap/DataLoader.java
@@ -28,19 +28,16 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 =  new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
         ownerService.save(owner1);
 
         Owner owner2 =  new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
         ownerService.save(owner2);
 
         Owner owner3 =  new Owner();
-        owner3.setId(3L);
         owner3.setFirstName("Michael");
         owner3.setLastName("Buble");
         ownerService.save(owner3);
@@ -49,13 +46,11 @@ public class DataLoader implements CommandLineRunner {
 
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Samuel");
         vet2.setLastName("Keathon");
         vetService.save(vet2);
