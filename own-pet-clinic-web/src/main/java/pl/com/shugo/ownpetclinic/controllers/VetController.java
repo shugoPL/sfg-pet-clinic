a945ec8c5ff25fc5427939bec640a58475f28c4c
shugoPL <shugoPL@users.noreply.github.com>
2022-11-23 09:37:11 +0100

    Added Vets List and updated controller. Close #15

--- own-pet-clinic-web/src/main/java/pl/com/shugo/ownpetclinic/controllers/VetController.java
+++ own-pet-clinic-web/src/main/java/pl/com/shugo/ownpetclinic/controllers/VetController.java
@@ -1,14 +1,24 @@
 package pl.com.shugo.ownpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.com.shugo.ownpetclinic.data.services.VetService;
 
 @RequestMapping("/vets")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
