a99ac1ac045557b19ab3b0459c949f4ffa058153
shugoPL <shugoPL@users.noreply.github.com>
2022-11-24 14:42:15 +0100

    added bootstrap PetType

--- own-pet-clinic-web/src/main/java/pl/com/shugo/ownpetclinic/bootstrap/DataLoader.java
+++ own-pet-clinic-web/src/main/java/pl/com/shugo/ownpetclinic/bootstrap/DataLoader.java
@@ -3,8 +3,10 @@ package pl.com.shugo.ownpetclinic.bootstrap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import pl.com.shugo.ownpetclinic.data.model.Owner;
+import pl.com.shugo.ownpetclinic.data.model.PetType;
 import pl.com.shugo.ownpetclinic.data.model.Vet;
 import pl.com.shugo.ownpetclinic.data.services.OwnerService;
+import pl.com.shugo.ownpetclinic.data.services.PetTypeService;
 import pl.com.shugo.ownpetclinic.data.services.VetService;
 
 
@@ -13,19 +15,28 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
-
-//    public DataLoader() {
-//        this.ownerService = new OwnerServiceMap();
-//        this.vetService = new VetServiceMap();
-//    }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
+
+
+
 
         Owner owner1 =  new Owner();
         owner1.setFirstName("Michael");
