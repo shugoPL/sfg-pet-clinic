51dd450a7707afcbe996baeebd22adc2ddbafe12
shugoPL <shugoPL@users.noreply.github.com>
2022-11-29 09:20:00 +0100

    added visit entity. close #38

--- own-pet-clinic-data/src/main/java/pl/com/shugo/ownpetclinic/data/model/Visit.java
+++ own-pet-clinic-data/src/main/java/pl/com/shugo/ownpetclinic/data/model/Visit.java
@@ -1,10 +1,19 @@
 package pl.com.shugo.ownpetclinic.data.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visits")
 public class Visit extends BaseEntity {
+
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "desc")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
